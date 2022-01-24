99748bf628116772fd5591992f2c66c070e4c99d
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-24 11:38:09 +0700

    update

--- lib/pages/row_column_demo.dart
+++ lib/pages/row_column_demo.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'my_card.dart';
+
 class RowColumnDemo extends StatelessWidget {
   const RowColumnDemo({Key? key}) : super(key: key);
 
@@ -24,61 +26,27 @@ class RowColumnDemo extends StatelessWidget {
                 icon: Icons.home,
               ),
               SizedBox(width: 12),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.alarm, color: Colors.white),
-                  SizedBox(height: 6),
-                  Text('Flutter', style: TextStyle(color: Colors.white)),
-                  Text('2.8', style: TextStyle(color: Colors.white)),
-                ],
+              MyCard(
+                title: 'NestJS',
+                subTitle: 'Version 2.8',
+                icon: Icons.person,
               ),
               SizedBox(width: 12),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.accessibility, color: Colors.white),
-                  SizedBox(height: 6),
-                  Text('React N', style: TextStyle(color: Colors.white)),
-                  Text('5.0', style: TextStyle(color: Colors.white)),
-                ],
+              MyCard(
+                title: 'Cats',
+                subTitle: 'Version 2.8',
+                icon: Icons.account_balance,
               ),
               SizedBox(width: 12),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.remove_done, color: Colors.white),
-                  SizedBox(height: 6),
-                  Text('React N', style: TextStyle(color: Colors.white)),
-                  Text('5.0', style: TextStyle(color: Colors.white)),
-                ],
+              MyCard(
+                title: 'React N',
+                subTitle: 'Version 0.66',
+                icon: Icons.person,
               ),
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class MyCard extends StatelessWidget {
-  final String? title;
-  final String? subTitle;
-  final IconData? icon;
-
-  const MyCard({Key? key, this.title, this.subTitle, this.icon})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: Colors.white),
-        SizedBox(height: 6),
-        Text(title!, style: TextStyle(color: Colors.white)),
-        Text(subTitle!, style: TextStyle(color: Colors.white)),
-      ],
     );
   }
 }
