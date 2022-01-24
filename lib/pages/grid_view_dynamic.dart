fcbc274153efa766f8dd6acc8c0cac2f3df8b4a6
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-24 15:57:46 +0700

    update

--- lib/pages/grid_view_dynamic.dart
+++ lib/pages/grid_view_dynamic.dart
@@ -19,6 +19,7 @@ List<CardModel> cards = [
   CardModel(title: 'title-2', subTitle: 'sub-2', icon: Icons.api),
   CardModel(title: 'title-3', subTitle: 'sub-3', icon: Icons.ac_unit),
   CardModel(title: 'title-4', subTitle: 'sub-4', icon: Icons.room),
+  CardModel(title: 'title-5', subTitle: 'sub-5', icon: Icons.air),
 ];
 
 class GridViewDynamic extends StatelessWidget {
@@ -31,33 +32,43 @@ class GridViewDynamic extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(20),
           child: GridView.count(
-            crossAxisCount: 2,
+            crossAxisCount: 3,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20,
             children: List.generate(cards.length, (index) {
               var model = cards[index];
-              return Container(
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                ),
-                padding: EdgeInsets.all(8),
-                child: Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    onTap: () {
-                      print(model.title);
-                    },
-                    child: MyCard(
-                      title: model.title,
-                      subTitle: model.subTitle,
-                      icon: model.icon,
-                    ),
-                  ),
-                ),
-              );
+              return CardAction(model: model);
             }),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardAction extends StatelessWidget {
+  final CardModel model;
+  const CardAction({Key? key, required this.model}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.purple,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      padding: EdgeInsets.all(8),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          onTap: () {
+            print(model.title);
+          },
+          child: MyCard(
+            title: model.title,
+            subTitle: model.subTitle,
+            icon: model.icon,
           ),
         ),
       ),
